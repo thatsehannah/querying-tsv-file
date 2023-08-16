1a667bc727ca8ceb956fd381deeabdab35a34c82
E. Hannah <echannah631@gmail.com>
2023-08-15 22:29:46 -0500

    created print functions, more linq practice

--- Program.cs
+++ Program.cs
@@ -14,28 +14,55 @@ namespace QueryingTsvFile
               .Select(line => Language.FromTsv(line))
               .ToList();
 
-            var prettifiedLanguages = languages.Select(l => l.Prettify());
-            //foreach (var l in prettifiedLanguages)
-            //{
-            //    Console.WriteLine(l);
-            //}
+            var prettifiedLanguages = languages.Select(l => l);
+            //PrintList(prettifiedLanguages);
 
             var csharpLanguages = from l in languages
                                   where l.Name == "C#" || l.Predecessors.Contains("C#")
-                                  select l.Prettify();
-            //foreach (var l in csharpLanguages)
-            //{
-            //    Console.WriteLine(l);
-            //}
+                                  select l;
+            //PrintList(csharpLanguages);
 
             var microsoftLanguages = from l in languages
                                      where l.ChiefDeveloper == "Microsoft"
-                                     select l.Prettify();
-            foreach (var l in microsoftLanguages)
+                                     select l;
+            //PrintList(microsoftLanguages);
+
+            var lispDescendants = from l in languages
+                                  where l.Predecessors.Contains("Lisp")
+                                  select l;
+            //PrintList(lispDescendants);
+
+            var scriptLangauges = from l in languages
+                                  where l.Name.Contains("Script")
+                                  select l;
+            //PrintList(scriptLangauges);
+
+            int languagesCount = languages.Count;
+            Console.WriteLine(languagesCount);
+
+            var nearMillenniumLangugages = from l in languages
+                                  where l.Year >= 1995 && l.Year <= 2005
+                                  select $"{l.Name} was invented in {l.Year}";
+            int nearMillenniumCount = nearMillenniumLangugages.Count();
+            Console.WriteLine(nearMillenniumCount);
+            PrintList(nearMillenniumLangugages);
+
+        }
+
+        static void PrintList(IEnumerable<Language> list)
+        {
+            foreach (var l in list)
+            {
+                Console.WriteLine(l.Prettify());
+            }
+        }
+
+        static void PrintList(IEnumerable<object> list)
+        {
+            foreach (var l in list)
             {
                 Console.WriteLine(l);
             }
-
         }
     }
 }
